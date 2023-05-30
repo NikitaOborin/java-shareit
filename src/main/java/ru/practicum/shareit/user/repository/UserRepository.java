908dd7dc158aa11676f77de46f557b8478ffbfd7
Oborin Nikita <oborinikita@yandex.ru>
2023-05-30 22:01:24 +0700

    feat: sprint#14, на первую проверку

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -1,19 +1,7 @@
 package ru.practicum.shareit.user.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.user.model.User;
 
-import java.util.List;
-
-public interface UserRepository {
-
-    User createUser(User user);
-
-    User updateUser(User user, Long id);
-
-    User getUser(Long id);
-
-    List<User> getAllUsers();
-
-    void deleteUser(Long id);
-
+public interface UserRepository extends JpaRepository<User, Long> {
 }
